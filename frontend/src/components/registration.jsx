749f969d41ea4cbac4ca0787e5dff62ed81cc2a8
Abhi Ram Dathu <lingarajuvelishala11@gmail.com>
2026-02-26 08:47:15 +0530

    changed

--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { API_BASE_URL } from '../config/apiBaseUrl';
-import '../styles/auth.css'
+import '../Styles/auth.css'
 
 
 /**
